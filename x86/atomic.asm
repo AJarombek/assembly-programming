dc91e8c255c790bfd2ad2faadb8b0f218b2c157e
ajarombek <ajarombek95@gmail.com>
2023-06-18 20:40:37 -0400

    refactor

--- x86/atomic.asm
+++ x86/atomic.asm
@@ -6,17 +6,15 @@ section .data
     ; Example integer value to operate on
     ; The 'dd' directive creates a double word (4 bytes) containing value 42 and stores it in the variable 'add_value'
     add_value dd 42
-    sub_value dd 40
 
+    ; Error message to print if the addition assertion fails
     add_error_message db 'Addition assertion failed!', 0
-    sub_error_message db 'Subtraction assertion failed!', 0
 
 section .text
     ; The 'global' directive makes the label 'main' available to the linker.
     global main
 
-extern __atomix_fetch_add
-extern __atomix_fetch_sub
+extern __atomic_fetch_add
 
 main:
     ; Set the value to add
@@ -25,8 +23,8 @@ main:
     ; Load the memory address of the value
     mov edi, add_value
 
-    ; Invoke __atomix_fetch_add
-    call __atomix_fetch_add
+    ; Invoke __atomic_fetch_add
+    call __atomic_fetch_add
 
     ; After the call, the updated value is stored in [edi]
     ; You can access the updated value and use it as needed
@@ -37,25 +35,6 @@ main:
     ; Conditional jump instruction that transfers the control flow to the assertion_failed label
     ; if the previous comparison (cmp) resulted in a "not equal" condition.
     jne assertion_failed_add
-
-    ; Set the value to subtract
-    mov esi, 10
-
-    ; Load the memory address of the value
-    mov edi, sub_value
-
-    ; Invoke __atomix_fetch_sub
-    call __atomix_fetch_sub
-
-    ; After the call, the updated value is stored in [edi]
-    ; You can access the updated value and use it as needed
-
-    ; Assertion: Check if the updated value is equal to 30
-    cmp dword [edi], 30
-
-    ; Conditional jump instruction that transfers the control flow to the assertion_failed label
-    ; if the previous comparison (cmp) resulted in a "not equal" condition.
-    jne assertion_failed_sub
 
     ; Exit the program
     mov eax, 1
@@ -78,23 +57,7 @@ assertion_failed_add:
     inc ebx
     int 0x80
 
-assertion_failed_sub:
-    ; Assertion failed, handle the failure (e.g., print an error message, exit program with an error code)
-
-    ; Print an error message
-    mov eax, 4  ; write system call
-    mov ebx, 1  ; file descriptor (stdout)
-    mov ecx, sub_error_message
-    mov edx, 30 ; message length
-    int 0x80
-
-    ; Exit the program with an error code
-    mov eax, 1
-    xor ebx, ebx
-    inc ebx
-    int 0x80
-
-__atomix_fetch_add:
+__atomic_fetch_add:
     ; Atomically adds the value in esi to the memory location pointed by rdi.
     ; The lock prefix ensures that the operation is atomic.
     ; The original value stored at the memory location is returned in eax.
@@ -102,22 +65,6 @@ __atomix_fetch_add:
 
     ; Doubles the value in esi.
     add esi, esi
-
-    ; Returns control from the function.
-    ret
-
-__atomix_fetch_sub:
-    ; Atomically subtracts the value in esi from the memory location pointed by rdi.
-    ; The lock prefix ensures that the operation is atomic.
-    ; The original value stored at the memory location is returned in eax.
-    ; Load the original value into eax
-    mov eax, [rdi]
-
-    ; Subtract the value in esi from eax atomically
-    lock sub [rdi], esi
-
-    ; Move the result to edi
-    mov edi, eax
 
     ; Returns control from the function.
     ret
